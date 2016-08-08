044e27f34bcd7319abe2a35d21896abbef9bbf91
Tarikur Rahaman <tarikur@w3engineers.com>
2016-08-08 19:00:00 +0600

    Make some changes

---
+++ ecr_repo_create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#################################################
+#												#
+# Purpose: ECS Cluster Initialize on aws        #
+# Maintainer: Tarikur Rahaman					#
+# Email : tarikur@w3engineers.com 				#
+# Create Date: 03-06-2016 						#
+# Last Modified: 								#
+#												#
+#################################################
